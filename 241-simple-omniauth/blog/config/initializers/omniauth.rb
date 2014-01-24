464dc97b50a07861f11be2687f6c6c3934b90231
Dan Thompson <dant1@stanford.edu>
2014-01-23 18:50:13 -0800

    set up omniauth to get tokens from google users. modified gmail gem to use oauth2 setting within gmail_xoauth. can now access gmail within console

--- 241-simple-omniauth/blog/config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "446049003901-t1pco3ufp89tuu9egptlpli88aescbae.apps.googleusercontent.com", "RDTaHv0XAtC3hIYemcsrDCxg", {
-    access_type: 'offline',
-    scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-    redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
-  }
-end
