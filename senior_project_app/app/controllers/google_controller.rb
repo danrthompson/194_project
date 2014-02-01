5f34578eb03298c2b570a25a3eb07290074d9b91
Dan Thompson <dant1@stanford.edu>
2014-01-31 16:13:38 -0800

    first pass at devise implementation

--- senior_project_app/app/controllers/google_controller.rb
+++ senior_project_app/app/controllers/google_controller.rb
@@ -1,10 +1,15 @@
+require 'awesome_print'
+
 class GoogleController < ApplicationController   
   def authenticate
     #What data comes back from OmniAuth?     
     @auth = request.env["omniauth.auth"]
     #Use the token from the data to request a list of calendars
-    @token = @auth["credentials"]["token"]
-    render text: @token and return
+    @token = @auth['credentials']['token']
+    @email = @auth['info']['email']
+    @name = @auth['info']['first_name']
+    # render text: ap(@auth) and return
+    # render text: @token and return
     # client = Google::APIClient.new
     # client.authorization.access_token = @token
     # service = client.discovered_api('calendar', 'v3')
@@ -12,5 +17,7 @@ class GoogleController < ApplicationController
     #   :api_method => service.calendar_list.list,
     #   :parameters => {},
     #   :headers => {'Content-Type' => 'application/json'})
+    gmail = Gmail.connect(:xoauth, @email, token: @token)
+    @count = gmail.inbox.count
   end
 end
