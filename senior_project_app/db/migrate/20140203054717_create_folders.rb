4aea96acedf08b365d5aef0cdeffd0abafa61945
Daniel Haarburger <work.haarburger@gmail.com>
2014-02-09 14:31:04 -0800

    committing before another pull

--- senior_project_app/db/migrate/20140203054717_create_folders.rb
+++ senior_project_app/db/migrate/20140203054717_create_folders.rb
@@ -1,11 +1,11 @@
 class CreateFolders < ActiveRecord::Migration
-  def change
-    create_table :folders do |t|
-      t.string :name
-      t.int :parent_id
-      t.int :user_id
+#   def change
+#     create_table :folders do |t|
+#       t.string :name
+#       t.int :parent_id
+#       t.int :user_id
 
-      t.timestamps
-    end
-  end
+#       t.timestamps
+#     end
+#   end
 end
