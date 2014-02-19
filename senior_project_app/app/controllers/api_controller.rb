43ff2934f892f43f7dd68e68c4a840db82f5bf95
Dan Thompson <dant1@stanford.edu>
2014-02-19 11:49:37 -0800

    compose email api implemented

--- senior_project_app/app/controllers/api_controller.rb
+++ senior_project_app/app/controllers/api_controller.rb
@@ -22,4 +22,41 @@ class ApiController < ApplicationController
 		end
 		render text: Email.email_array_to_json(conversation.emails.order(:date).reverse_order)
 	end
+
+	def send_email
+		user = User.find_by_id(params[:user_id])
+		if not user then
+			render text: 'Error, user doesn\'t exist.' and return
+		end
+		if params[:to].blank? or params[:subject].blank? then
+			render text: 'To field and subject field required.' and return
+		end
+		user.refresh_token_if_necessary
+		gmail = connect_to_gmail(user)
+
+		to_data = params[:to]
+		subject_data = params[:subject]
+		body_data = params[:body]
+		cc_data = (!params[:cc].blank?) ? params[:cc] : nil
+		bcc_data = (!params[:bcc].blank?) ? params[:bcc] : nil
+		gmail.deliver! do
+			to to_data
+			subject subject_data
+			text_part do
+				body body_data
+			end
+			if cc_data then
+				cc cc_data
+			end
+			if bcc_data then
+				bcc bcc_data
+			end
+		end
+
+		render text: 'Email sent.'
+	end
+
+	def connect_to_gmail(user)
+		Gmail.connect!(:xoauth, user.email, token: user.auth_token)
+	end
 end
