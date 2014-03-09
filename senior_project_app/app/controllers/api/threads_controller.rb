ba87ef06b9e8ea7c548411e6d54903d4fc375f85
Dan Thompson <dant1@stanford.edu>
2014-03-09 15:56:09 -0700

    pushing partial edits

---
+++ senior_project_app/app/controllers/api/threads_controller.rb
@@ -0,0 +1,23 @@
+class Api::ThreadsController < ApplicationController
+	before_filter :authenticate_user!
+
+	def create
+
+	end
+
+	def show
+
+	end
+
+	def reply
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+end
